98c6d4d1af96d3ff3ea522e86e87533a6c355872
Pablo Soto Gonzalez <pablosotogonzalez@MacBook-Pro-de-Pablo.local>
2021-10-29 13:53:58 +0200

    cdsi

--- ft_printf.c
+++ ft_printf.c
@@ -6,17 +6,11 @@
 /*   By: psoto-go <psoto-go@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 15:39:25 by psoto-go          #+#    #+#             */
-/*   Updated: 2021/10/28 17:32:00 by psoto-go         ###   ########.fr       */
+/*   Updated: 2021/10/29 13:53:40 by psoto-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-int	ft_putchar_fd(char c, int fd)
-{
-	write(fd, &c, 1);
-	return(0);
-}
 
 size_t	ft_strlen(const char *s)
 {
@@ -28,21 +22,88 @@ size_t	ft_strlen(const char *s)
 	return (count);
 }
 
-int	ft_putstr_fd(char *s)
+void	ft_putnbr_fd(int n, int fd)
 {
-	int		i;
-	int		len;
-	
-	i = 0;
-	if (!s)
-		return(write(1, "(null)", 6));
-	len = ft_strlen(s);
-	while (i < len)
+	if (n > -2147483648 || n <= 2147483647)
 	{
-		ft_putchar_fd(s[i],1);
+		if (n == -2147483648)
+		{
+			ft_putchar_fd('-', fd);
+			ft_putchar_fd('2', fd);
+			ft_putnbr_fd(147483648, fd);
+		}
+		else if (n >= 10)
+		{
+			ft_putnbr_fd(n / 10, fd);
+			ft_putnbr_fd(n % 10, fd);
+		}
+		else if (n < 0)
+		{
+			n = -n;
+			ft_putchar_fd('-', fd);
+			ft_putnbr_fd(n, fd);
+		}
+		else
+		{
+			ft_putchar_fd(n + '0', fd);
+		}
+	}
+}
+
+
+void	*ft_memcpy(void *str1, const void *str2, size_t n)
+{
+	unsigned char	*temp1;
+	unsigned char	*temp2;
+	size_t			count;
+
+	temp1 = (unsigned char *)str1;
+	temp2 = (unsigned char *)str2;
+	count = 0;
+	if (str1 == NULL && str2 == NULL)
+		return (NULL);
+	while (count < n)
+	{	
+		temp1[count] = temp2[count];
+		count++;
+	}
+	return (str1);
+}
+
+
+char	*ft_strdup(const char *src)
+{
+	char			*aux;
+	size_t			len;
+
+	len = ft_strlen(src) + 1;
+	aux = (char *)malloc(len * sizeof(char));
+	if (aux == NULL)
+		return (NULL);
+	ft_memcpy(aux, src, len);
+	return (aux);
+}
+
+
+int	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+	return (0);
+}
+
+static int ft_print(char *c)
+{
+	char 	*aux;
+	int		i;
+
+	aux = ft_strdup(c);
+	i = 0;
+	while(aux[i] != '\0')
+	{
+		write(1, &aux[i], 1);
 		i++;
 	}
-	return (i);
+	return(0);
 }
 
 int ft_comprueba(const char arg1, const char arg2, va_list args)
@@ -51,30 +112,18 @@ int ft_comprueba(const char arg1, const char arg2, va_list args)
 
 	res = 0;
 	if (arg1 == '%' && arg2 == 'c')
-	{
-		res = ft_putchar_fd(va_arg(args, int),1);
-	}
+		ft_putchar_fd(va_arg(args, int), res);
 	else if (arg1 == '%' && arg2 == 's')
-	{
-		res = ft_putstr_fd(va_arg(args, char));
-		// printf("%s", va_arg(args, char));
-		// write(tmp[0], va_arg(args, char), 1);
-	}
+		res = ft_print(va_arg(args, char *));
 	else if (arg1 == '%' && arg2 == 'p')
 	{
 		// printf("%p", va_arg(args, char));
 		// write(tmp[0], va_arg(args, char), 1);
 	}
 	else if (arg1 == '%' && arg2 == 'd')
-	{
-		// printf("%d", va_arg(args, int));
-		// ft_putnbr_fd(va_arg(args, long), &res);
-	}
+		ft_putnbr_fd(va_arg(args, int), res);
 	else if (arg1 == '%' && arg2 == 'i')
-	{
-		// printf("%i", va_arg(args, char));
-		// write(tmp[0], va_arg(args, char), 1);
-	}
+		ft_putnbr_fd(va_arg(args, int), res);
 	return(res);
 	
 }
@@ -91,14 +140,23 @@ int ft_printf(const char *var, ...)
 
 	while (var[i] != '\0')
 	{
-		res += ft_comprueba(var[i], var[i + 1], args);
+		if(var[i] == '%')
+		{
+			res += ft_comprueba(var[i], var[i + 1], args);
+			i++;
+		}else
+		{
+			write(1, &var[i], 1);
+		}
 		i++;
+		
 	}
 	va_end(args);
 	return (res);
 }
 
 int	main(){
-	printf("%s\n", "'aa'");
-	ft_printf("%s", "aa");
+	printf("%s %d\n", "hello", 0);
+	ft_printf("%s %d\n", "hello", 0);
+	printf("%s %d\n", "hello", 0);
 }
